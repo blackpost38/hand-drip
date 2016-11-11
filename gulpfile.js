fd4af29a011b324449dbcd1edffb65acbf09182f
blackpost38 <blackpost38@gmail.com>
2016-11-11 20:23:39 +0900

    Set browser-sync in gulpfile
    to reload automatically and test multi-device, browser-sync is set by gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,35 @@ var browserify = require('browserify');
 var stringify = require('stringify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var browserSync = require('browser-sync').create();
 
-
-gulp.task("transform", function () {
+gulp.task('transform-js', function () {
 	var b = browserify('./js/main.js')
 		.transform(babelify, {presets: ['es2015']})
 		.transform(stringify, {
 			appliesTo: { includeExtensions: ['.html'] },
 			minify: true
-    })
+    });
 
   return b.bundle()
 		.pipe(source('main.js'))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['transform']);
+gulp.task('js-watch', ['transform-js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('browser-sync', function () {
+	browserSync.init({
+		server: { baseDir: './' }
+	});
+});
+
+gulp.task('source-watch', function () {
+	gulp.watch(['js/*.js', 'js/**/*.js'], ['js-watch']);
+	gulp.watch('*.html').on('change', browserSync.reload);
+})
+
+gulp.task('default', ['browser-sync', 'source-watch']);
